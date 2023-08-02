507c797d4a1306b7cd99a9213e86b5fbb1f57f68
KostasKv <kostas.kvietinskas@gmail.com>
2023-08-02 14:55:56 +0100

    Added responsive masonry layout for project cards

--- script.js
+++ script.js
@@ -31,3 +31,45 @@ window.addEventListener("scroll", () => {
     ? avatar.classList.add("hideAvatar")
     : avatar.classList.remove("hideAvatar");
 });
+
+const projectCards = document.querySelector(".project__cards");
+
+// Using Masonry plugin to achieve masonry layout for project cards
+// See https://masonry.desandro.com/
+var masonry = new Masonry(projectCards, {
+  itemSelector: ".project__card",
+  fitWidth: true,
+  gutter: 128,
+});
+
+function matchProjectCardSizeDependingOnWindowSize() {
+  var width = NaN;
+
+  if (window.innerWidth > 1400) {
+    width = "576px";
+  } else if (window.innerWidth > 550) {
+    width = "445px";
+  } else if (window.innerWidth > 374) {
+    width = "335px";
+  } else {
+    width = "300px";
+  }
+
+  var cards = document.querySelectorAll(".project__card");
+
+  cards.forEach((card) => {
+    card.style.maxWidth = width;
+  });
+}
+
+// Layout may load before images do. This ensures the layout
+// is updated once the project card sizes are correct
+imagesLoaded(projectCards, () => {
+  matchProjectCardSizeDependingOnWindowSize();
+  masonry.layout();
+});
+
+window.addEventListener("resize", (event) => {
+  matchProjectCardSizeDependingOnWindowSize();
+  masonry.layout();
+});
