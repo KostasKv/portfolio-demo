73c1826399dc291fbf40c7e861c339b10926ad6f
KostasKv <kostas.kvietinskas@gmail.com>
2023-08-17 15:57:03 +0100

    Load More btn functionality & extra projects added

--- src/app/projects-display.js
+++ src/app/projects-display.js
@@ -1,24 +1,36 @@
 "use client";
 import { useState } from "react";
 
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import styles from "./projects-display.module.css";
-import anotherOneImg from "@public/another-one.png";
+import shopifyCloneImg from "@public/shopify-clone.png";
 import blenderRendersImg from "@public/blender-renders.png";
 import kotoStudioImg from "@public/koto-studio.png";
 import switchBlackImg from "@public/switch-black.png";
-import uideliImg from "@public/uideli.png";
+import stackitImg from "@public/stackit.png";
 import remarketImg from "@public/remarket.png";
+import appMarketingPageImg from "@public/app-marketing-page.jpg";
+import audiProductPageImg from "@public/audi-product-page.jpg";
+import digitalProductMarketingImg from "@public/digital-product-marketing.jpg";
+import flowshapeImg from "@public/flowshape.jpg";
+import meditationAppImg from "@public/meditation-app.jpg";
+import ourdateImg from "@public/ourdate.jpg";
+import slotbloxImg from "@public/slotblox.jpg";
+import webDesignTrendsImg from "@public/web-design-trends.jpg";
+
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import { CategorySelector } from "./category-selector";
 import { ProjectCard } from "./project-card";
 
 export function ProjectsDisplay() {
+  const INITIAL_MAX_ITEMS = 6;
+
   const [category, setCategory] = useState("all");
+  const [maxItems, setMaxItems] = useState(INITIAL_MAX_ITEMS);
 
   const projects = [
     {
-      src: uideliImg,
-      title: "UIDeli.com",
+      src: stackitImg,
+      title: "Stackit",
       categories: ["design", "development"],
       year: 2023,
     },
@@ -41,8 +53,8 @@ export function ProjectsDisplay() {
       year: 2023,
     },
     {
-      src: anotherOneImg,
-      title: "Another One",
+      src: shopifyCloneImg,
+      title: "Shopify Clone",
       categories: ["development"],
       year: 2023,
     },
@@ -52,10 +64,73 @@ export function ProjectsDisplay() {
       categories: ["design", "development"],
       year: 2023,
     },
+    {
+      src: appMarketingPageImg,
+      title: "App Marketing Page",
+      categories: ["design"],
+      year: 2022,
+    },
+    {
+      src: audiProductPageImg,
+      title: "Audi Product Page",
+      categories: ["design", "development"],
+      year: 2022,
+    },
+    {
+      src: digitalProductMarketingImg,
+      title: "Digital Product Marketing",
+      categories: ["design", "development"],
+      year: 2022,
+    },
+    {
+      src: flowshapeImg,
+      title: "FlowShape",
+      categories: ["design"],
+      year: 2021,
+    },
+    {
+      src: meditationAppImg,
+      title: "Meditation App",
+      categories: ["development"],
+      year: 2021,
+    },
+    {
+      src: ourdateImg,
+      title: "Our Date",
+      categories: ["development"],
+      year: 2020,
+    },
+    {
+      src: slotbloxImg,
+      title: "SlotBlox",
+      categories: ["design"],
+      year: 2019,
+    },
+    {
+      src: webDesignTrendsImg,
+      title: "Web Design Trends",
+      categories: ["design", "development"],
+      year: 2019,
+    },
   ];
+
+  function onCategorySelect(newCategory) {
+    // Switching category should reset the number of projects shown
+    setMaxItems(INITIAL_MAX_ITEMS);
+    setCategory(newCategory);
+  }
+
+  function increaseNumProjectsDisplayed() {
+    const MAX_ITEMS_INCREMENT = 4;
+
+    if (maxItems < projects.length) {
+      setMaxItems(maxItems + MAX_ITEMS_INCREMENT);
+    }
+  }
 
   var filteredProjects;
 
+  // Filter by selected category
   if (["design", "development"].includes(category)) {
     filteredProjects = projects.filter((projectInfo) => {
       return projectInfo.categories.includes(category);
@@ -64,15 +139,20 @@ export function ProjectsDisplay() {
     filteredProjects = projects;
   }
 
-  const projectItems = filteredProjects.map((projectInfo) => (
-    <ProjectCard key={projectInfo.title} {...projectInfo} />
+  var canLoadMoreProjects = filteredProjects.length > maxItems;
+
+  // Limit number of items to display
+  filteredProjects = filteredProjects.slice(0, maxItems);
+
+  const projectItems = filteredProjects.map((projectInfo, index) => (
+    <ProjectCard key={index} {...projectInfo} />
   ));
 
   return (
     <>
       <CategorySelector
         selected={category}
-        onSelect={(newCategory) => setCategory(newCategory)}
+        onSelect={(newCategory) => onCategorySelect(newCategory)}
       />
 
       <div className={styles.cardsContainer}>
@@ -80,7 +160,16 @@ export function ProjectsDisplay() {
           <Masonry gutter={"3rem"}>{projectItems}</Masonry>
         </ResponsiveMasonry>
       </div>
-      <button className={styles.btnAccent}>Load more</button>
+
+      {canLoadMoreProjects ? (
+        <button
+          className={styles.btnAccent}
+          onClick={increaseNumProjectsDisplayed}>
+          Load more
+        </button>
+      ) : (
+        ""
+      )}
     </>
   );
 }
